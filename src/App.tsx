c0f05f49e1080321595a7df8f29c7d7a112d7d43
WhiteWolfWCY <mateuszmartyna44@gmail.com>
2026-01-04 15:27:39 +0100

    init: Setup of the gsap and tailwind; Initial css styling

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import gsap from 'gsap'
+import  { ScrollTrigger, SplitText} from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='flex-center'>
+            Hello
+        </h1>
+    </div>
+  )
+}
+
+export default App
